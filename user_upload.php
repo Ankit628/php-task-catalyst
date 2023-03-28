9636e54468954f8d5e1fa03e53feaeca4e54e7cc
Ankit Subedi <ankitsubedi15@outlook.com>
2023-03-28 20:29:06 +1100

    Database connection module copied from helper

--- user_upload.php
+++ user_upload.php
@@ -1,1 +1,14 @@
 <?php
+// Define database connection variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "catalyst_db";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check database connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
